5dfd86368310e83930baf01df55e7bbabf0e13f6
김동규 <54929514+Donggggg@users.noreply.github.com>
2021-06-02 11:40:32 +0900

    Initial Commit

---
+++ 3DVirtualHumanModeling/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  3DVirtualHumanModeling
+//
+//  Created by 김동규 on 2021/06/02.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
